1470eab0c35c276e940e66403b6d7e56a49af156
WASEEM ATTAR <waseem_6957@yahoo.com>
2022-12-13 10:19:17 +0530

    Delete src directory

--- src/Validator.java
+++
@@ -1,8 +0,0 @@
-import java.util.regex.Pattern;
-
-public class Validator {
-    public boolean isValidRegistrationNumber(String reg_no) {
-        boolean b3 = (Pattern.matches("\\d{4}", reg_no) && !(reg_no.equals("0000")));
-        return b3;
-    }
-}
